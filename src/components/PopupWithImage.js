a098c74f0157d19ee99635e33ef06e91fca52857
Daniil Kopylov <Shinoinochi@yandex.ru>
2023-04-23 10:13:21 +0500

    на проверку

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,9 +3,10 @@ export default class PopupWithImage extends Popup {
     constructor(popup) {
         super(popup);
         this._popupImageSelector = this._popup.querySelector('.popup__image');
+        this._popupImageTitle = this._popup.querySelector('.popup-image__title');
     }
     open(data) {
-        this._popup.querySelector('.popup-image__title').textContent = data.name;
+        this._popupImageTitle.textContent = data.name;
         this._popupImageSelector.src = data.link;
         this._popupImageSelector.setAttribute('alt', data.name);
         super.open();
